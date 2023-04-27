f1effce82802e5c064c00b19dda51a9ff59485ec
shettysho <131759919+shettysho@users.noreply.github.com>
2023-04-27 14:22:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+        height="100vh"
+        projectID="b57f63c9-a566-4e63-8785-f3fe2bb48d3a"
+        userName="javascriptmastery"
+        userSecret="123123"
+        />
+        )
+}
+
+export default App;
+
+
